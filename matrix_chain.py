e99c55fd877ececc3250423aba8ec2ebe267f411
TamojitaChatterjee <150126862+TamojitaChatterjee@users.noreply.github.com>
2025-04-12 15:24:16 +0530

    matrix_chain.py

--- matrix_chain.py
+++ matrix_chain.py
@@ -1,4 +1,8 @@
 #matrix chain multiplication using dynamic programming
+'''
+You have been given a chain of matrices, and you need to find out “what is the minimum number of scalar multiplications needed to multiply these matrices?”. 
+It is to be noted that since matrix multiplication is associative, so the order in which we are multiplying the matrices will determine the number of multiplications needed. 
+'''
 def matrix_chain_multiplication(dims):
     n = len(dims) - 1  # Number of matrices
     dp = [[0] * n for _ in range(n)]  # dp[i][j] stores min cost of multiplying matrix i to j
