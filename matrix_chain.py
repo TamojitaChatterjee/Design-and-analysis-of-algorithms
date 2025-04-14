6cef8fff28f097fc4af882bad34c21ae1bad4cc8
TamojitaChatterjee <150126862+TamojitaChatterjee@users.noreply.github.com>
2025-04-14 21:07:56 +0530

    Update matrix_chain.py

--- matrix_chain.py
+++ matrix_chain.py
@@ -3,16 +3,46 @@
 You have been given a chain of matrices, and you need to find out “what is the minimum number of scalar multiplications needed to multiply these matrices?”. 
 It is to be noted that since matrix multiplication is associative, so the order in which we are multiplying the matrices will determine the number of multiplications needed. 
 '''
-def matrix_chain_multiplication(dims):
-    n = len(dims) - 1  # Number of matrices
-    dp = [[0] * n for _ in range(n)]  # dp[i][j] stores min cost of multiplying matrix i to j
+import math
+def matrix_chain_multiplication ( dimensions : list [ int ]) -> tuple [int , list [tuple [int , int ]]]:
+    
+        
+    n = len( dimensions ) - 1 # Number of matrices
+    dp = [[0 for _ in range ( n + 1) ] for _ in range ( n + 1) ]
+    S = [[0 for _ in range ( n + 1) ] for _ in range ( n + 1) ]
+    ## YOUR CODE GOES HERE
 
-    for l in range(2, n + 1):  # l is chain length
-        for i in range(n - l + 1):
-            j = i + l - 1
-            dp[i][j] = float('inf')
-            for k in range(i, j):
-                cost = dp[i][k] + dp[k + 1][j] + dims[i] * dims[k + 1] * dims[j + 1]
-                dp[i][j] = min(dp[i][j], cost)
+    for d in range(1,n+1):
+        for i in range(1,n-d+1):
+            j=i+d
+            m=[-1,-1]
+            for k in range(i,j):
+                cost=dp[i][k]+dp[k+1][j]+dimensions[i-1]*dimensions[k]*dimensions[j]
+                if m[0]==-1 or cost<m[0]:
+                    m[0]=cost
+                    m[1]=k
+            dp[i][j]=m[0]
+            S[i][j]=m[1]
 
-    return dp[0][n - 1]
+    print("no. of computation: ",dp[1][n])
+## Print the minimum number of multiplications involved .
+
+    split=[]
+
+    def mul_matrices(i,j):
+        if i==j:
+            return
+        k=S[i][j]
+        split.append((i,j))
+        mul_matrices(i,k)
+        mul_matrices(k+1,j)
+
+    mul_matrices(1,n)
+
+    return (dp[1][n],split)
+
+
+dimensions = [30, 35, 15, 5, 10, 20, 25]
+max_cost, split_list = matrix_chain_multiplication(dimensions)
+print("Max number of multiplications:", max_cost)
+print("Split positions (i, j):", split_list)
